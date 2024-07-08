99366e01f57194c1017fc3bce5b19a4a39cfda9e
njoromyke <mykenjoroge01@gmail.com>
2024-07-08 12:37:14 +0300

    chore: Update error middleware to use statusCodes utility

--- middlewares/error.middleware.ts
+++ middlewares/error.middleware.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-
-const statusCodes = require("../utils/status-codes");
+import statusCodes from "../utils/status-codes.util";
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
@@ -10,8 +9,8 @@ const notFound = (req: Request, res: Response, next: NextFunction) => {
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   const statusCode = res.statusCode === statusCodes.OK ? statusCodes.INTERNAL_SERVER_ERROR : res.statusCode;
-  res.status(statusCode);
-  res.json({
+
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
   });
