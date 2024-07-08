4911dda537045823fc8fda191cda3e614d12f462
njoromyke <mykenjoroge01@gmail.com>
2024-07-08 15:06:40 +0300

    implement journal route

--- routes/journal.route.ts
+++ routes/journal.route.ts
@@ -1,16 +1,12 @@
 import express from "express";
-import { createJournal, getJournals, getJournalById, deleteJournal, updateJournal } from "../controllers/journal.controller";
+import { createJournal, getJournalById, getMyJournals, updateJournal } from "../controllers/journal.controller";
 import protect from "../middlewares/auth.middleware";
-import { createJournalValidator, updateJournalValidator } from "../validations/journal.validation";
+import { createJournalValidator } from "../validations/journal.validation";
 
 const router = express.Router();
 
-router.route("/").post(protect, createJournalValidator, createJournal).get(protect, getJournals);
+router.route("/").post(protect, createJournalValidator, createJournal).get(protect, getMyJournals);
 
-router
-  .route("/:id")
-  .get(protect, getJournalById)
-  .put(protect, updateJournalValidator, updateJournal)
-  .delete(protect, deleteJournal);
+router.route("/:id").get(protect, getJournalById).put(protect, updateJournal);
 
 module.exports = router;
