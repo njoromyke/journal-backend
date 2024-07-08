8a44da7e844e87e1e8715004f4e9c2d8c9eff6c1
njoromyke <mykenjoroge01@gmail.com>
2024-07-08 15:06:55 +0300

    refactor: Refactor dateFilter function for clarity and readability

--- utils/date.util.ts
+++ utils/date.util.ts
@@ -1,4 +1,5 @@
 const filters = ["today", "this_week", "this_month"];
+
 const dateFilter = (dateFilter: string): { startDate: Date; endDate: Date } => {
   const currentDate = new Date();
   let startDate: Date;
@@ -6,23 +7,28 @@ const dateFilter = (dateFilter: string): { startDate: Date; endDate: Date } => {
 
   switch (dateFilter) {
     case filters[0]:
-      startDate = new Date(currentDate.setHours(0, 0, 0, 0));
+      startDate = new Date(currentDate);
+      startDate.setHours(0, 0, 0, 0);
       endDate = new Date(startDate);
       endDate.setDate(startDate.getDate() + 1);
       break;
+
     case filters[1]:
-      const weekStart = currentDate.getDate() - currentDate.getDay();
-      startDate = new Date(currentDate.setDate(weekStart));
-      startDate.setHours(0, 0, 0, 0);
+      const weekStart = new Date(currentDate);
+      weekStart.setDate(currentDate.getDate() - currentDate.getDay());
+      startDate = new Date(weekStart.setHours(0, 0, 0, 0));
       endDate = new Date(startDate);
       endDate.setDate(startDate.getDate() + 7);
       break;
+
     case filters[2]:
       startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
       endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
       break;
+
     default:
-      startDate = new Date(currentDate.setHours(0, 0, 0, 0));
+      startDate = new Date(currentDate);
+      startDate.setHours(0, 0, 0, 0);
       endDate = new Date(startDate);
       endDate.setDate(startDate.getDate() + 1);
       break;
